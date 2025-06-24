9316955d4a7a3e7e6c0a5badf2ce95499b8814ab
Muhammad Faisal Khan <49272303+Faisalkhan2019@users.noreply.github.com>
2025-06-25 00:46:25 +0500

    Added Array JS file (updated)

--- Arrays.js
+++ Arrays.js
@@ -4,7 +4,7 @@
 
 let arr = ["Faisal", "Anam", "Hamdan", "Salwa", "Anaya"];
 
-const arr2 = [0, 1, 2, 3, 4];
+const arr2 = [0, 1, "Faisal", 3, 4];
 const arr3 = new Array(0, 1, 2, 3);
 
 // console.log(typeof arr3);
@@ -13,14 +13,14 @@ const arr3 = new Array(0, 1, 2, 3);
 // Arrays Methods
 
 // arr2.push(5) // add 5 at the end of array
-// arr2.push(66) // add 5 (elem) at the end of array
+// arr2.push(66) // add 66 (elem) at the end of array
 // arr2.pop() // Removes the last element from an array
 // arr2.unshift("AD Khan", "Khan") //Adds one or more elements to the beginning of an array.
 // arr2.shift() //Removes the first element from an array.
 
-// console.log(arr2.includes(3))// The includes() method of Array instances determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+// console.log(arr2.includes(3))// true The includes() method of Array instances determines whether an array includes a certain value among its entries, returning true or false as appropriate.
 
-// console.log(arr2.indexOf(2)); //The indexOf() method of Array instances returns the first index at which a given element can be found in the array, or -1 if it is not present.
+// console.log(arr2.indexOf("Khan")); //The indexOf() method of Array instances returns the first index at which a given element can be found in the array, or -1 if it is not present.
 
 const myArr = arr2.join();
 // console.log(myArr)
@@ -50,9 +50,7 @@ const myNewArry = ["Faisal", "Anam", "Anaya", "Salwa", "Hamdan"];
 
 // The splice() method adds and/or removes array elements. The splice() method overwrites the original array.
 
-const myNArr = myNewArry.splice(0, 1, "Lahore");
-
-// console.log(myNArr);
+myNewArry.splice(2, 1, "Lahore");
 // console.log(myNewArry);
 
 
@@ -86,11 +84,65 @@ for(x = 0; x< myBooks.length; x+=3){
 myBooks.reverse(); // to reverse order array elements from bottom to top
 
 for (let books of myBooks){
-    console.log(books);
+    // console.log(books);
     
 }
 
 
+// Arrays - Part 2
+
+
+const myHeros = ["Quaid e Azam", "Alama Iqbal", "Imran Khan"]
+const fruits = ["Banana", "Orange", "Kiwi"]
+// myHeros.push(fruints) // Array fruits to be added inside the myHeros array as a array element.
+// console.log(myHeros)
+// console.log(myHeros[3][2]); // Kiwi
+
+// const myBothArr = myHeros.concat(fruits)
+
+// The concat() method concatenates (joins) two or more arrays and returns a new array, containing the joined arrays. method does not change the existing arrays.
+
+// console.log(myBothArr);
+
+const mySpreadArr = [...myHeros, ...fruits] // this is spread operator method in arrays to join or merge two arrays 
+// console.log(mySpreadArr); // Same Result as per cancat()  but this is useful method
+
+
+
+const arr1 = [0, 1, 2, [3, 4], [5,6,7],8];
+
+// console.log(arr1.flat());
+// console.log(arr1[4]);
+// expected output: Array [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+const arr4 = [0, 1, [2, [3, [4, 5]]]];
+
+// console.log(arr4.flat()); 
+// expected output: Array [0, 1, 2, Array [3, Array [4, 5]]]
+
+// console.log(arr4.flat(2)); // defined depth
+// expected output: Array [0, 1, 2, 3, Array [4, 5]]
+
+// console.log(arr4.flat(Infinity)); // we dont need to defined depth you can just write (Infinity).
+// expected output: Array [0, 1, 2, 3, 4, 5]
+
+
+// ------------------ //
+
+// const myNewArray = "Faisal"
+// console.log(Array.isArray(myNewArray)) //false
+// console.log(Array.from("Faisal")); //[ 'F', 'a', 'i', 's', 'a', 'l' ]
+// console.log(Array.from(myNewArray)); //[ 'F', 'a', 'i', 's', 'a', 'l' ]
+
+
+// console.log(Array.from({name: "Faisal"})); // [] but Interesting need to learn this method
+
+const score1 = 100
+const score2 = 200
+const score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+// Expected Output: [ 100, 200, 300 ]
 
 
 
