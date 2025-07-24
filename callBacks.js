4dc80ed91e7f7344290d771fda7d1e5fc7854c58
Muhammad Faisal Khan <49272303+Faisalkhan2019@users.noreply.github.com>
2025-07-24 23:49:44 +0500

    Call Back Functions in Javascript (basic)

---
+++ callBacks.js
@@ -0,0 +1,83 @@
+// // A JavaScript callback function is a function passed as an argument to another function, and it is executed after the main function completes.
+// // This is very useful for tasks like:
+// // Executing code after a task is finished
+// // Handling asynchronous operations like API calls, file loading, or events
+
+
+
+// // hello();
+// // goodbye();
+
+// // function hello(){
+// //     setTimeout(function (){
+// //     console.log("Hello World");
+// // }, 3000)
+    
+    
+// // }
+
+
+// // function goodbye(){
+// //     console.log("GoodBye!");
+    
+// // }
+
+
+
+
+// // hello(wait);
+
+
+// // function hello(myCall){
+// //     console.log("Hello World");
+// //     myCall()
+// // }
+
+
+// // function goodbye(){
+// //     console.log("GoodBye!");
+// // }
+// // function wait(){
+// //     console.log("Wait");
+// // }
+
+// // sum(displayResult, 100, 200)
+
+// // function sum(callback, x, y){
+// //     let result = x + y;
+// //     callback(result)
+// // }
+
+// // function displayResult(result){
+// //     console.log(result);
+    
+// // }
+
+// // console.log("One");
+// // console.log("Three");
+
+
+// // function myCall(){
+// //     console.log("Two");
+    
+// // }
+// // setTimeout(myCall, 2000)
+
+
+// // function sum(a, b){
+// // console.log(a + b);
+// // }
+
+// // function calculator (a, b, sumCal){
+// //     sumCal(a,b)
+   
+    
+// // }
+// // calculator(100,200, sum)
+
+// const name = ()=>{
+//     console.log("My Name is Hamdan Khan");
+    
+// }
+// setTimeout(name, 2000)
+
