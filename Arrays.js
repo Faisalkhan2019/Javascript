6cbbf148441d6bb36f35dd3c5af46b248a679df6
Muhammad Faisal Khan <49272303+Faisalkhan2019@users.noreply.github.com>
2025-07-24 22:34:28 +0500

    Arrays Update file

--- Arrays.js
+++ Arrays.js
@@ -4,7 +4,7 @@
 
 let arr = ["Faisal", "Anam", "Hamdan", "Salwa", "Anaya"];
 
-const arr2 = [0, 1, "Faisal", 3, 4];
+const arr2 = [0, 1, "Faisal", 3, 4, "Khan"];
 const arr3 = new Array(0, 1, 2, 3);
 
 // console.log(typeof arr3);
@@ -18,12 +18,14 @@ const arr3 = new Array(0, 1, 2, 3);
 // arr2.unshift("AD Khan", "Khan") //Adds one or more elements to the beginning of an array.
 // arr2.shift() //Removes the first element from an array.
 
-// console.log(arr2.includes(3))// true The includes() method of Array instances determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+// console.log(arr2.includes(5))// "true" The includes() method of Array instances determines whether an array includes a certain value among its entries, returning true or false as appropriate.
 
 // console.log(arr2.indexOf("Khan")); //The indexOf() method of Array instances returns the first index at which a given element can be found in the array, or -1 if it is not present.
 
 const myArr = arr2.join();
 // console.log(myArr)
+// console.log(typeof myArr);
+
 
 // The join() method of Array instances creates and returns a new string by concatenating all of the elements in this array, separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator.
 
@@ -44,14 +46,14 @@ const myNewArry = ["Faisal", "Anam", "Anaya", "Salwa", "Hamdan"];
 
 // const myNArr = myNewArry.slice(1, 3)
 
-// console.log(myNewArry);
-
 // console.log(myNArr);
+
 
 // The splice() method adds and/or removes array elements. The splice() method overwrites the original array.
 
 myNewArry.splice(2, 1, "Lahore");
 // console.log(myNewArry);
+
 
 
 // for loop in Array 
@@ -65,16 +67,22 @@ const myBooks = [
   "English"
 ];
 
+
+
 // method - 1
 
 for (i = 0; i < myBooks.length; i++) {
   const elem = myBooks[i];
-//   console.log(`${i}: ${elem}`);
+  // console.log(`${i}: ${elem}`);
 }
 
+for( ar = 0; ar <myBooks.length; ar++){
+  // console.log(myBooks[ar]);
+  
+}
 // method - 2
 
-for(x = 0; x< myBooks.length; x+=3){
+for(x = 0; x< myBooks.length; x+=2){
     // console.log(myBooks[x])
 }
 
@@ -94,17 +102,18 @@ for (let books of myBooks){
 
 const myHeros = ["Quaid e Azam", "Alama Iqbal", "Imran Khan"]
 const fruits = ["Banana", "Orange", "Kiwi"]
-// myHeros.push(fruints) // Array fruits to be added inside the myHeros array as a array element.
+const myNum = [1,2,4,5]
+// myHeros.push(fruits) // Array fruits to be added inside the myHeros array as a array element.
 // console.log(myHeros)
 // console.log(myHeros[3][2]); // Kiwi
 
-// const myBothArr = myHeros.concat(fruits)
+const myBothArr = myHeros.concat(fruits)
 
 // The concat() method concatenates (joins) two or more arrays and returns a new array, containing the joined arrays. method does not change the existing arrays.
 
 // console.log(myBothArr);
 
-const mySpreadArr = [...myHeros, ...fruits] // this is spread operator method in arrays to join or merge two arrays 
+const mySpreadArr = [...myHeros, ...fruits, ...myNum] // this is spread operator method in arrays to join or merge two or arrays 
 // console.log(mySpreadArr); // Same Result as per cancat()  but this is useful method
 
 
@@ -129,9 +138,9 @@ const arr4 = [0, 1, [2, [3, [4, 5]]]];
 
 // ------------------ //
 
-// const myNewArray = "Faisal"
+const myNewArray = "Hamdan"
 // console.log(Array.isArray(myNewArray)) //false
-// console.log(Array.from("Faisal")); //[ 'F', 'a', 'i', 's', 'a', 'l' ]
+// console.log(Array.from("Hamdan")); //[ 'F', 'a', 'i', 's', 'a', 'l' ]
 // console.log(Array.from(myNewArray)); //[ 'F', 'a', 'i', 's', 'a', 'l' ]
 
 
@@ -139,10 +148,19 @@ const arr4 = [0, 1, [2, [3, [4, 5]]]];
 
 const score1 = 100
 const score2 = 200
-const score3 = 300
+const score3 = 300 
 
-console.log(Array.of(score1,score2,score3));
+// console.log(score1,score2,score3);
+// console.log(Array.of(score1,score2,score3));
 // Expected Output: [ 100, 200, 300 ]
+
+
+
+console.log(Object.keys({name: "Faisal"})); 
+console.log(Object.values({name: "Faisal"})); 
+// Output: [['name', 'Faisal']]
+
+
 
 
 
